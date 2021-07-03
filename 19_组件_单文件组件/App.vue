136e8f217ac5cab21e765ee9c98269a9bad1b46d
xiaoliu66 <lunhuilk@163.com>
2021-07-03 21:25:15 +0800

    组件_单文件组件

---
+++ 19_组件_单文件组件/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <School />
+    </div>
+</template>
+
+<script>
+import School from "./components/School.vue";
+
+export default {
+    components: { SchoolSchool },
+};
+</script>
---
+++ 19_组件_单文件组件/components/School.vue
@@ -0,0 +1,36 @@
+/* 配置模板结构 */
+<template>
+    <div>
+        <h2 class="title">学校名：{{ name }}</h2>
+        <h2 class="info" @click="showAddress">学校地址：{{ address }}</h2>
+    </div>
+</template>
+
+/* 配置组件数据、交互、事件等等 */
+<script>
+// 此处只是暴露了一个组件的配置，并没有去创建组件。没有调用Vue.extend()
+export default {
+    data() {
+        return {
+            name: "光明小学",
+            address: "xxxx",
+        };
+    },
+    methods: {
+        showAddress() {
+            console.log(this);
+        }
+    },
+};
+</script>
+
+/* 配置组件样式*/
+<style lang="css">
+    .title{
+        background-color: aqua;
+    }
+
+    .info{
+        background-color: orangered;
+    }
+</style>
